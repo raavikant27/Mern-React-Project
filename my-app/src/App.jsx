e5b013bb288930505f71c6c2c1238a8b2b99029f
Ravikant singh <sravikant302@gmail.com>
2025-08-10 02:23:42 +0530

    setup

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Body from './components/Body';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+export default function App() {
+  return (
+    <>
+      <Body/>
+      <Footer/>
+      <Header/>
+    </>
+  );
+}
