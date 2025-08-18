82be618f00c2c2f25a35f794fbbb9ef562886650
Ravikant singh <sravikant302@gmail.com>
2025-08-18 22:40:20 +0530

    card-completed

--- my-app/src/components/Body.jsx
+++ my-app/src/components/Body.jsx
@@ -10,6 +10,10 @@ function Body() {
       <div className='res-container'>  
         <RestaurantCard/>
         <RestaurantCard/>
+         <RestaurantCard/>
+        <RestaurantCard/>
+         <RestaurantCard/>
+        <RestaurantCard/>
       </div>
     </div>
   );
