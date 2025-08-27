67683518b41197d4d412d98772a1d3c55f08b0b8
Ravikant singh <sravikant302@gmail.com>
2025-08-28 01:51:34 +0530

    footer

--- my-app/src/components/Footer.jsx
+++ my-app/src/components/Footer.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="footer">
+      <div className="footer-content">
+        <p>© {new Date().getFullYear()} RadiantNeuron. All rights reserved.</p>
+        <div className="footer-links">
+          <a
+            href="https://www.linkedin.com/in/ravikant-singh-2100b2266/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin size={28} />
+          </a>
+          <a
+            href="https://github.com/raavikant27"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub size={28} />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
