18665097d3daf1f581c9d5aabe4f026f72d74855
Ravikant singh <sravikant302@gmail.com>
2025-08-30 04:30:12 +0530

    creating a state

--- my-app/src/components/User.jsx
+++ my-app/src/components/User.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function User({name}) {
-   return <div className="user-card">
 
+  const [count]=useState(0);
+   return <div className="user-card">
+<h1>count={count}</h1>
    <h2>Name :{name}</h2>
    <h3>Location : Vns</h3>
    <h4>Contact : 7052513167</h4>
--- my-app/src/components/UserClass.jsx
+++ my-app/src/components/UserClass.jsx
@@ -4,13 +4,18 @@ class UserClass extends React.Component {
  constructor(props){
     super(props);
     console.log(props);
+     // way to create state varible 
+    this.state = {
+    count:0,
+ };
  }
+
 
 
 
     render(){
        return <div className="user-card">
-
+      <h1>Count : {this.state.count}</h1>
    <h2>Name :{this.props.name}</h2>
    <h3>Location : {this.props.location}</h3>
    <h4>Contact : 7052513167</h4>
