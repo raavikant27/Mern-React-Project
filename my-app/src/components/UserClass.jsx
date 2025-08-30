d6b061bc57f00fc205b632f26b7cc444918fbff2
Ravikant singh <sravikant302@gmail.com>
2025-08-30 13:27:56 +0530

    setstate in class based components

--- my-app/src/components/UserClass.jsx
+++ my-app/src/components/UserClass.jsx
@@ -1,28 +1,41 @@
 import React from "react";
 
 class UserClass extends React.Component {
- constructor(props){
+  constructor(props) {
     super(props);
     console.log(props);
-     // way to create state varible 
+    // way to create state variable
     this.state = {
-    count:0,
- };
- }
+      count: 0,
+      count2: 2,
+    };
+  }
 
+  render() {
+    // ✅ Destructure props and state inside render()
+    const { name, location, count } = this.props;
+    const { count2 } = this.state;
 
-
-
-    render(){
-       return <div className="user-card">
-      <h1>Count : {this.state.count}</h1>
-   <h2>Name :{this.props.name}</h2>
-   <h3>Location : {this.props.location}</h3>
-   <h4>Contact : 7052513167</h4>
-  
-   
- </div>
-    }
-
+    return (
+      <div className="user-card">
+        <h1>Count : {this.state.count}</h1>
+        <button
+          onClick={() => {
+            // ✅ never update state variable directly
+            this.setState({
+              count: this.state.count + 1,
+            });
+          }}
+        >
+          Count Increase
+        </button>
+        <h2>Name: {name}</h2>
+        <h3>Location: {location}</h3>
+        <h4>Contact: 7052513167</h4>
+        <h5>Count2 : {count2}</h5>
+      </div>
+    );
+  }
 }
+
 export default UserClass;
